4c9925342c9be88e825ca9ce97d3699e61c72296
Andy <130807763+joy199703021@users.noreply.github.com>
2023-12-11 18:33:39 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,14 +5,15 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://www.duyou.life' },
+    { title: '人一天至少要醒来两次，先从睡梦中醒来，再从习性中醒来', url: 'https://www.duyou.life' }
+    { title: '老一辈总说到什么时间就该做什么事，唯独不提到点下班', url: 'https://www.duyou.life' }
   ],
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '与你分享',
   HEO_HERO_TITLE_2: '高手的黑箱',
-  HEO_HERO_TITLE_3: 'Duyou.life/',
+  HEO_HERO_TITLE_3: 'Duyou.life',
   HEO_HERO_TITLE_4: '数字时代',
   HEO_HERO_TITLE_5: '聚焦于真正有价值的信息',
   HEO_HERO_TITLE_LINK: 'https://www.duyou.life/',
@@ -37,7 +38,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://www.duyou.life/about', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -103,7 +104,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://www.duyou.life/about',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
